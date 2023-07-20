8fd4133498fb930055bae43de6b7f8c0dfca9cd7
SayyedSha <93458712+SayyedSha@users.noreply.github.com>
2023-07-20 17:12:02 +0530

    Update fullscrapping.py

--- fullscrapping.py
+++ fullscrapping.py
@@ -1,18 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from db_con import get_review_summary
+from db_con import get_review_summary,con2
 import re
 import time
 import undetected_chromedriver as uc
 from bs4 import BeautifulSoup
-# conn = con2()
-# cursor = conn.cursor()
 
+conn = con2()
+cursor = conn.cursor()
 
-driver= webdriver.Edge()
+driver = webdriver.Chrome()
 driver.maximize_window()
-# driver = webdriver.Chrome()
-url = f"https://www.google.com/search?q=werq+labs&sxsrf=AB5stBgPduxXVm-0SzcQ6rAQ4Rd4hMU8Ow%3A1689814822665&source=hp&ei=Joe4ZKG-JufkseMPu8W_mAw&iflsig=AD69kcEAAAAAZLiVNgY4WXKLAn5Tz7c_RzuH65WwbDL1&gs_ssp=eJzj4tVP1zc0LC7IrUrLSck1YLRSNagwTko1TzZMMU81NzFJszRKsTKoSDUzTDYwNkoxTE1LNTFJSvXiLE8tKlTISUwqBgCG7hPr&oq=w&gs_lp=Egdnd3Mtd2l6IgF3KgIIADINEC4YrwEYxwEYigUYJzIEECMYJzIHECMYigUYJzIHEAAYigUYQzIHEAAYigUYQzIHEAAYigUYQzILEAAYgAQYsQMYgwEyCxAAGIAEGLEDGIMBMgsQABiABBixAxiDATINEAAYigUYsQMYgwEYQ0iCCFAAWABwAHgAkAEAmAGcAaABnAGqAQMwLjG4AQPIAQD4AQE&sclient=gws-wiz"
+url = f"https://www.google.com/search?q=DR.JOHN+FOX&hl=en&biw=1536&bih=763&sxsrf=AB5stBgPpR_2kcqGVNSxGEpAk5I00Jg7RA%3A1689845712005&ei=z_-4ZI3-POS02roPxuK2-AI&ved=0ahUKEwjNoY7r_ZyAAxVkmlYBHUaxDS8Q4dUDCA8&uact=5&oq=DR.JOHN+FOX&gs_lp=Egxnd3Mtd2l6LXNlcnAiC0RSLkpPSE4gRk9YMgUQABiABDIGEAAYFhgeMgYQABgWGB4yBhAAGBYYHjIGEAAYFhgeMgYQABgWGB4yBhAAGBYYHjIGEAAYFhgeMgYQABgWGB4yBhAAGBYYHkjXC1CCBFiCBHABeAGQAQCYAcYBoAHGAaoBAzAuMbgBA8gBAPgBAvgBAcICChAAGEcY1gQYsAPiAwQYACBBiAYBkAYD&sclient=gws-wiz-serp"
 driver.get(url)
 
 block= driver.find_element(By.CSS_SELECTOR,'#rhs > div > div > div.I6TXqe')
@@ -32,11 +31,13 @@ review_block=driver.find_element(By.CSS_SELECTOR,"#QA0Szd > div > div > div.w6VY
 review_div=review_block.find_element(By.XPATH,'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[3]/div/div/button[2]/div[2]/div[2]').click()
 time.sleep(5)#wait for page load
 
+
+
 R = driver.find_element(By.CSS_SELECTOR,"#QA0Szd > div > div > div.w6VYqd > div:nth-child(2) > div > div.e07Vkf.kA9KIf > div > div > div.m6QErb.DxyBCb.kA9KIf.dS8AEf")
-print(R.text)
+# print(R.text)
 
 R1=R.find_element(By.CSS_SELECTOR,"div:nth-child(8)")
-print(R1.text)
+
 
 Rlist=R.find_elements(By.XPATH,'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[3]/div[9]')
 
@@ -52,11 +53,17 @@ for i in Rlist:
         review=i.find_elements(By.XPATH,'//*[@class="MyEned"]/span')
 
 response = BeautifulSoup(driver.page_source, 'html.parser')
-# reviews = response.find_all('div', class_='m6QErb')
-re_b = response.find_all('div',class_='jftiEf fontBodyMedium')
-print(re_b)
-review_set =get_review_summary(re_b)
-print(review_set)
+
+reviews = response.find_all('div',class_='jftiEf fontBodyMedium')
+
+review_set =get_review_summary(reviews)
+merged_lists = [list(t) for t in zip(review_set['Reviewer_name'], review_set['Review Rate'], review_set['Review Time'], review_set['Review Text'])]
+
+
+for item in merged_lists:
+    cursor.callproc('sp_dummy_insert',tuple(item))
+    conn.commit()
+    time.sleep(3)
 
 def get_review_summary(result_set):
     rev_dict = {
@@ -68,10 +75,13 @@ def get_review_summary(result_set):
         review_name=result.find('div',class_='d4r55').text
         review_rate = result.find('span', class_='kvMYJc')["aria-label"]
         review_time = result.find('span',class_='rsqaWe').text
-        review_text = result.find('span',class_='wiI7pd').text
+        review_text = result.find('span',class_='wiI7pd')
+        if review_text:
+            r_text=review_text.text
+        else:
+            r_text=""
         rev_dict['Reviewer_name'].append(review_name)
         rev_dict['Review Rate'].append(review_rate)
         rev_dict['Review Time'].append(review_time)
-        rev_dict['Review Text'].append(review_text)
-    import pandas as pd    
-    return(pd.DataFrame(rev_dict))
+        rev_dict['Review Text'].append(r_text) 
+    return(rev_dict)
