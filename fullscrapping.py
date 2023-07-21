743c2a02079362c065b786ee7dbf1a9ef0329582
SayyedSha <93458712+SayyedSha@users.noreply.github.com>
2023-07-21 16:11:39 +0530

    Update fullscrapping.py

--- fullscrapping.py
+++ fullscrapping.py
@@ -6,82 +6,78 @@ import time
 import undetected_chromedriver as uc
 from bs4 import BeautifulSoup
 
+#Data_Base Connector
 conn = con2()
-cursor = conn.cursor()
+cursor = conn.cursor()#Connecting cursor
 
-driver = webdriver.Chrome()
-driver.maximize_window()
-url = f"https://www.google.com/search?q=DR.JOHN+FOX&hl=en&biw=1536&bih=763&sxsrf=AB5stBgPpR_2kcqGVNSxGEpAk5I00Jg7RA%3A1689845712005&ei=z_-4ZI3-POS02roPxuK2-AI&ved=0ahUKEwjNoY7r_ZyAAxVkmlYBHUaxDS8Q4dUDCA8&uact=5&oq=DR.JOHN+FOX&gs_lp=Egxnd3Mtd2l6LXNlcnAiC0RSLkpPSE4gRk9YMgUQABiABDIGEAAYFhgeMgYQABgWGB4yBhAAGBYYHjIGEAAYFhgeMgYQABgWGB4yBhAAGBYYHjIGEAAYFhgeMgYQABgWGB4yBhAAGBYYHkjXC1CCBFiCBHABeAGQAQCYAcYBoAHGAaoBAzAuMbgBA8gBAPgBAvgBAcICChAAGEcY1gQYsAPiAwQYACBBiAYBkAYD&sclient=gws-wiz-serp"
-driver.get(url)
+#Calling Store Procedure to get data
+cursor.callproc('sp_dummy_dr_get')
+result2 = list(cursor.stored_results())[0].fetchall()
+cursor.nextset()
 
-block= driver.find_element(By.CSS_SELECTOR,'#rhs > div > div > div.I6TXqe')
-print(block)
+#Looping Through 
+for i in range(len(result2)):
+    url=result2[i][1] 
+    id_value=result2[i][0]
 
-#Scrapping values of ratings and reviews
-values = block.find_element(By.XPATH,'//*[@class="osrp-blk"]/div[1]/div[2]/div[2]').text.replace('\n'," ")
-Rating  = values.split(" ")[0] 
-total_review  = values.split(" ")[1]
-print(f'Rating : '+ Rating + '\n'+f'Total_review : '+total_review)
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    # url = f"https://www.google.com/search?q=DR.JOHN+FOX&hl=en&biw=1536&bih=763&sxsrf=AB5stBgPpR_2kcqGVNSxGEpAk5I00Jg7RA%3A1689845712005&ei=z_-4ZI3-POS02roPxuK2-AI&ved=0ahUKEwjNoY7r_ZyAAxVkmlYBHUaxDS8Q4dUDCA8&uact=5&oq=DR.JOHN+FOX&gs_lp=Egxnd3Mtd2l6LXNlcnAiC0RSLkpPSE4gRk9YMgUQABiABDIGEAAYFhgeMgYQABgWGB4yBhAAGBYYHjIGEAAYFhgeMgYQABgWGB4yBhAAGBYYHjIGEAAYFhgeMgYQABgWGB4yBhAAGBYYHkjXC1CCBFiCBHABeAGQAQCYAcYBoAHGAaoBAzAuMbgBA8gBAPgBAvgBAcICChAAGEcY1gQYsAPiAwQYACBBiAYBkAYD&sclient=gws-wiz-serp"
+    driver.get(url)
 
-map_block=block.find_element(By.XPATH,'//*[@class="osrp-blk"]/div[1]/div[1]/div')
-map=map_block.find_element(By.XPATH,'//*[@id="lu_map"]').click()
-time.sleep(5)#wait for page load
+    block= driver.find_element(By.CSS_SELECTOR,'#rhs > div > div > div.I6TXqe')
+    print(block)
 
-review_block=driver.find_element(By.CSS_SELECTOR,"#QA0Szd > div > div > div.w6VYqd > div:nth-child(2)")
-review_div=review_block.find_element(By.XPATH,'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[3]/div/div/button[2]/div[2]/div[2]').click()
-time.sleep(5)#wait for page load
+    #Scrapping values of ratings and reviews
+    values = block.find_element(By.XPATH,'//*[@class="osrp-blk"]/div[1]/div[2]/div[2]').text.replace('\n'," ")
+    Rating  = values.split(" ")[0] 
+    total_review  = values.split(" ")[1]
+    print(f'Rating : '+ Rating + '\n'+f'Total_review : '+total_review)
 
+    map_block=block.find_element(By.XPATH,'//*[@class="osrp-blk"]/div[1]/div[1]/div')
+    map=map_block.find_element(By.XPATH,'//*[@id="lu_map"]').click()
+    time.sleep(5)#wait for page load
 
-
-R = driver.find_element(By.CSS_SELECTOR,"#QA0Szd > div > div > div.w6VYqd > div:nth-child(2) > div > div.e07Vkf.kA9KIf > div > div > div.m6QErb.DxyBCb.kA9KIf.dS8AEf")
-# print(R.text)
-
-R1=R.find_element(By.CSS_SELECTOR,"div:nth-child(8)")
-
-
-Rlist=R.find_elements(By.XPATH,'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[3]/div[9]')
-
-
-for i in Rlist:
-    try:
-        i.find_element(By.XPATH,'//*[@class="MyEned"]/span[2]/button').click()
-        review=i.find_elements(By.XPATH,'//*[@class="MyEned"]/span')
-        
-    except:
-        
-        name=i.find_elements(By.XPATH,'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[3]/div[9]/div/div/div/div[2]/div[2]/div[1]/button/div[1]')
-        review=i.find_elements(By.XPATH,'//*[@class="MyEned"]/span')
-
-response = BeautifulSoup(driver.page_source, 'html.parser')
-
-reviews = response.find_all('div',class_='jftiEf fontBodyMedium')
-
-review_set =get_review_summary(reviews)
-merged_lists = [list(t) for t in zip(review_set['Reviewer_name'], review_set['Review Rate'], review_set['Review Time'], review_set['Review Text'])]
+    review_block=driver.find_element(By.CSS_SELECTOR,"#QA0Szd > div > div > div.w6VYqd > div:nth-child(2)")
+    review_div=review_block.find_element(By.XPATH,'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[3]/div/div/button[2]/div[2]/div[2]').click()
+    time.sleep(5)#wait for page load
 
 
-for item in merged_lists:
-    cursor.callproc('sp_dummy_insert',tuple(item))
-    conn.commit()
-    time.sleep(3)
 
-def get_review_summary(result_set):
-    rev_dict = {
-        'Reviewer_name':[],
-        'Review Rate': [],
-        'Review Time': [],
-        'Review Text' : []}
-    for result in result_set:
-        review_name=result.find('div',class_='d4r55').text
-        review_rate = result.find('span', class_='kvMYJc')["aria-label"]
-        review_time = result.find('span',class_='rsqaWe').text
-        review_text = result.find('span',class_='wiI7pd')
-        if review_text:
-            r_text=review_text.text
-        else:
-            r_text=""
-        rev_dict['Reviewer_name'].append(review_name)
-        rev_dict['Review Rate'].append(review_rate)
-        rev_dict['Review Time'].append(review_time)
-        rev_dict['Review Text'].append(r_text) 
-    return(rev_dict)
+    R = driver.find_element(By.CSS_SELECTOR,"#QA0Szd > div > div > div.w6VYqd > div:nth-child(2) > div > div.e07Vkf.kA9KIf > div > div > div.m6QErb.DxyBCb.kA9KIf.dS8AEf")
+    # print(R.text)
+
+    # R1=R.find_element(By.CSS_SELECTOR,"div:nth-child(8)")
+
+
+    Rlist=R.find_elements(By.XPATH,'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[3]/div[9]')
+
+
+    for i in Rlist:
+        try:
+            i.find_element(By.XPATH,'//*[@class="MyEned"]/span[2]/button').click()
+            review=i.find_elements(By.XPATH,'//*[@class="MyEned"]/span')
+            
+        except:
+            
+            name=i.find_elements(By.XPATH,'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[3]/div[9]/div/div/div/div[2]/div[2]/div[1]/button/div[1]')
+            review=i.find_elements(By.XPATH,'//*[@class="MyEned"]/span')
+
+    response = BeautifulSoup(driver.page_source, 'html.parser')
+
+    reviews = response.find_all('div',class_='jftiEf fontBodyMedium')
+
+    review_set =get_review_summary(reviews)
+    merged_lists = [(id_value, *t) for t in zip(review_set['Reviewer_name'], review_set['Review Rate'], review_set['Review Time'], review_set['Review Text'])]
+
+    print(merged_lists)
+
+    # for item in merged_lists:
+    #     cursor.callproc('sp_dummy_insert',tuple(item))
+    #     conn.commit()
+    #     time.sleep(2)
+    
+    time.sleep(5)
+    driver.quit()
+    
+    time.sleep(10)
